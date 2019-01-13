f17565061b39be1e6648f311fddd2d00b3584eb2
Dennis Yu <dennisyuxx@hotmail.com>
2019-01-12 21:47:20 -0500

    filter player data for SoloQ - id:420

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const async = require('async');
+const PORT = process.env.PORT || 8000;
 const {apiKey} = require('./config.json');
 
 app.use(express.json());
@@ -9,38 +10,38 @@ app.use(express.json());
 axios.defaults.headers.common['X-Riot-Token'] = apiKey;
 
 app.post('/result', (req, res) => {
-    const url = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/';
-    const check_match = 'https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/';
+    const check_summoner_history = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/';
+    const check_match_by_name = 'https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/';
+    const check_match_by_id = 'https://na1.api.riotgames.com/lol/match/v4/matches/';
+    let queue_type = '?queue=420';
     let player1_name = req.body.player1_name;
     let player2_name = req.body.player2_name;
 
     async.waterfall([
         function(callback) {
-            axios.get(url+player1_name).then((response) => {
+            axios.get(check_summoner_history + player1_name).then((response) => {
                 callback(null, response.data)
             });            
         },
         function(player1_data, callback) {
-            axios.get(url+player2_name).then((response) => {
+            axios.get(check_summoner_history + player2_name).then((response) => {
                 callback(null, player1_data, response.data)
             });   
         },
         function(player1_data, player2_data, callback) {
             let player1_accountId = player1_data.accountId;
 
-            axios.get(check_match + player1_accountId).then((response) => {
+            axios.get(check_match_by_name + player1_accountId + queue_type).then((response) => {
                 callback(null, response.data, player2_data);
             })
         },
         function(player1_data, player2_data, callback) {
             let player2_accountId = player2_data.accountId;
 
-            axios.get(check_match + player2_accountId).then((response) => {
+            axios.get(check_match_by_name + player2_accountId + queue_type).then((response) => {
                 callback(null, {
-                    result: {
-                        player1_data,
-                        player2_data: response.data
-                    }
+                    player1_data,
+                    player2_data: response.data
                 })
             })
         }
@@ -50,6 +51,10 @@ app.post('/result', (req, res) => {
     })
 })
 
-app.listen(8000, () => {    
-    console.log('Server is running')
+// app.get('/match', (req, res) => {
+//     const url = 'https://na1.api.riotgames.com/lol/match/v4/matches/2948248611';
+//     axios.get(url).then((response) => res.send(response.data))
+// })
+app.listen(PORT, () => {    
+    console.log(`Server is running on port:${PORT}`)
 });
