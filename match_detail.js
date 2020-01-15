639808828f145a76fd33189a7c6988cfedb8cc02
Dennis Yu <dennisyuxx@hotmail.com>
2020-01-15 14:48:55 -0500

    parallel call to reduce wait time & add rateLimit, concurrencyCallLimit

--- match_detail.js
+++ match_detail.js
@@ -17,45 +17,75 @@ async function match_api(player_name, matchId) {
     return detail
 }
 
-async function match_detail(matchId, player_name, accountId) {
-    const summoner = await Summoner.findOne({
+function fetch_detail(summoner, matchId, player_name, index) {
+    return new Promise(async (resolve, reject) => {
+        if (!_.find(summoner.matches, ['gameId', _.toString(matchId[index])])) {
+            let detail = await match_api(player_name, matchId[index])
+            summoner.matches.push({
+                gameId: matchId[index],
+                match: detail
+            })
+            summoner.lastUpdated = Math.floor(Date.now() / 1000);
+            console.log(`${matchId[index]} saved`)
+            resolve(summoner);
+        } else {
+            console.log(`${matchId[index]} already exists`)
+            reject();
+        }
+    })
+}
+
+function limitCall(summoner, matchId, player_name, rateLimit, index ,concurrentCallLimit, timeInterval) {
+    return new Promise(async (resolve, reject) => {
+        let promises = [];
+        let currentLimit = 1;
+        while (currentLimit <= rateLimit) {
+            let promise = fetch_detail(summoner, matchId, player_name, index);
+            promises.push(promise)
+            if (currentLimit % concurrentCallLimit === 0 || currentLimit === rateLimit) {
+                await Promise.all(promises)
+                    .then(() => {
+                        if (currentLimit === rateLimit){
+                            return;
+                        }
+                        console.log(`${timeInterval}ms`)
+                        return new Promise(resolve => setTimeout(resolve, timeInterval));
+                    })
+                    .catch(() => currentLimit--)
+                promises = [];
+            }
+            currentLimit++;
+            index++;
+        }
+        summoner.lastIndex = index;
+        resolve(summoner);
+    })
+}
+
+async function match_detail(matchId, player_name, accountId, rateLimit) {
+    let summoner = await Summoner.findOne({
         summonerId: accountId
     })
-
     // if summoner doesnt exist in db
     if (!summoner) {
-        let detail = await match_api(player_name, matchId)
-        await Summoner.create({
+        let detail = await match_api(player_name, matchId[0])
+        summoner = await Summoner.create({
             summonerId: accountId,
             matches: {
-                gameId: matchId,
+                gameId: matchId[0],
                 match: detail
             },
-            lastUpdated: Math.floor(Date.now() / 1000)
+            lastUpdated: Math.floor(Date.now() / 1000),
+            lastIndex: 1
         })
-    } else {
-        // if summoner exist in db & match detail is not yet saved
-        if (!_.find(summoner.matches, ['gameId', _.toString(matchId)])) {
-            let detail = await match_api(player_name, matchId)
-            // console.log(result)
-            summoner.matches.push({
-                gameId: matchId,
-                match: detail
-            })
-            summoner.lastUpdated = Math.floor(Date.now() / 1000)
-
-            summoner.save((error) => {
-                if (error) {
-                    console.log('error')
-                }
-                console.log(`${matchId} saved`);
-            })
-        } else {
-            console.log(`${matchId} already exists`)
-        }
+        console.log(`${matchId[0]} created`);
+        rateLimit -= 1;
     }
-    // })
-    return result
+
+    // if summoner exist in db & match detail is not yet saved
+    summoner = await limitCall(summoner, matchId, player_name, rateLimit, summoner.lastIndex, 5, 1000)
+    await summoner.save()
+    console.log(`all saved`);
 }
 
 module.exports = { match_detail }
