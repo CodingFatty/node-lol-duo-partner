fcac15dabdf5193f37b3f514c2465660cd23ff05
Dennis Yu <dennisyuxx@hotmail.com>
2020-01-20 01:46:50 -0500

    update db

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const { db } = require('../config.js');
 
-const mongoDB = 'mongodb://localhost/duo_analysis';
+const mongoDB = `mongodb://${db.username}>:${db.password}>@ds263068.mlab.com:63068/duo-analysis`;
 
 mongoose.set('useCreateIndex', true)
 mongoose.connect(mongoDB, { useNewUrlParser: true });
