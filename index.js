3661a18f9c3fd51ccc7d26fa277d068be953cdb9
Dennis Yu <dennisyuxx@hotmail.com>
2020-01-19 23:34:06 -0500

    add cors & try catch on index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const cors = require('cors');
 const _ = require('lodash');
+const mongoose = require('./db/connect');
 const PORT = process.env.PORT || 8000;
 const { apiKey } = require('./config.json');
 const { summoner_id } = require('./summoner_id');
@@ -9,40 +11,46 @@ const { match_list } = require('./match_list');
 const { match_detail } = require('./match_detail');
 const { summoner_stat } = require('./summoner_stat');
 
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 axios.defaults.headers.common['X-Riot-Token'] = apiKey;
 axios.defaults.headers.common['Accept-Charset'] = "application/x-www-form-urlencoded";
 
 app.post('/result', async (req, res) => {
-    let { player1_name, player2_name } = req.body;
+    try {
+        let { player1_name, player2_name } = req.body;
 
-    // // getting summoner ID
-    const player1_info = await summoner_id(player1_name)
-    const player2_info = await summoner_id(player2_name)
-    await Promise.all([player1_info, player2_info])
+        // getting summoner ID
+        const player1_info = await summoner_id(player1_name)
+        const player2_info = await summoner_id(player2_name)
+        await Promise.all([player1_info, player2_info])
 
-    // // get match history
-    let player1_history = await match_list(player1_info.accountId);
-    let player2_history = await match_list(player2_info.accountId);
-    await Promise.all([player1_history, player2_history]);
+        // get match history
+        let player1_history = await match_list(player1_info.accountId);
+        let player2_history = await match_list(player2_info.accountId);
+        await Promise.all([player1_history, player2_history])
 
-    // fetch match detail
-    let player1_matchId = player1_history.map(history => history.gameId)
-    let player2_matchId = player2_history.map(history => history.gameId)
-    await Promise.all([
-        match_detail(player1_matchId, player1_info.name, player1_info.accountId, 20),
-        match_detail(player2_matchId, player2_info.name, player2_info.accountId, 20)])
+        // fetch match detail
+        let player1_matchId = player1_history.map(history => history.gameId)
+        let player2_matchId = player2_history.map(history => history.gameId)
+        await Promise.all([
+            match_detail(player1_matchId, player1_info.name, player1_info.accountId, 20),
+            match_detail(player2_matchId, player2_info.name, player2_info.accountId, 20)])
 
 
-    // extract summoner stat
-    let p1_result = await summoner_stat(player1_info.accountId)
-    let p2_result = await summoner_stat(player2_info.accountId)
+        // extract summoner stat
+        let p1_result = await summoner_stat(player1_info.accountId)
+        let p2_result = await summoner_stat(player2_info.accountId)
 
-    res.status(200).json({
-        'Player1' : p1_result,
-        'Player2' : p2_result
-    })
+        res.status(200).json({
+            'Player1': p1_result,
+            'Player2': p2_result
+        })
+    } catch (e) {
+        res.status(400).send(e)
+    }
 })
 
 app.listen(PORT, () => {
