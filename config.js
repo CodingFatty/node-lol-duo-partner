e4f773d3efaa2dc13994d6b10642cec29395258b
Dennis Yu <dennisyuxx@hotmail.com>
2020-01-26 00:51:35 -0500

    update env variables

---
+++ config.js
@@ -0,0 +1,8 @@
+let env = process.env.NODE_ENV || 'development';
+
+if (env === 'development'){
+    let config = require('./config.json');
+    Object.keys(config).forEach((key) => {
+        process.env[key] = config[key];
+      });
+}
